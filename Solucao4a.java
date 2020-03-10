c38da2226729595550f3bbd80450faccdcdae3f0
João Victor Brandão dos Reis Silva - 81723059 <81723059@animaedu.intranet>
2020-03-10 17:47:49 -0300

    Mostra peso

---
+++ Solucao4a.java
@@ -0,0 +1,10 @@
+public class Solucao4a
+{
+    public static void main (String... args)
+    {
+        String peso = javax.swing.JOptionPane.showInputDialog("Digite um numero");
+        javax.swing.JOptionPane.showMessageDialog(null,"o peso informado foi " + peso + "Kg");
+
+    }
+
+}
