cbab00e068cd4d6418b484cac8bec23138cc5eac
João Victor Brandão dos Reis Silva - 81723059 <81723059@animaedu.intranet>
2020-03-10 17:46:48 -0300

    Le um numero inteiro e mostra na tela

---
+++ Solucao2a.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Solucao2a
+{
+   public static void main(String[] args)
+   {
+      String valor;
+      valor = JOptionPane.showInputDialog("Digite um numero:");
+      JOptionPane.showMessageDialog (null,"O valor do numero digitado é: " + valor);
+   }
+}
